0960ba2a9db4cc7db7126f75609a94947233d97d
marlont63 <tavarez.marlon@gmail.com>
2018-03-15 20:51:05 +0100

    Initial Commit

---
+++ Working ScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Working ScrollView
+//
+//  Created by Marlon Raschid Tavarez Parra on 15/3/18.
+//  Copyright © 2018 es.mybcloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Working ScrollViewTests/Working_ScrollViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Working_ScrollViewTests.swift
+//  Working ScrollViewTests
+//
+//  Created by Marlon Raschid Tavarez Parra on 15/3/18.
+//  Copyright © 2018 es.mybcloud. All rights reserved.
+//
+
+import XCTest
+@testable import Working_ScrollView
+
+class Working_ScrollViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
