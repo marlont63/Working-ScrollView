6231330af9da87e4e5e40130a315c3544110c6a7
marlont63 <tavarez.marlon@gmail.com>
2018-03-19 20:34:43 +0100

    Primer commit
    Trabajando con el ScrollView creando un carousel de tarjetas

--- Working ScrollView/ViewController.swift
+++ Working ScrollView/ViewController.swift
@@ -9,17 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var card:[String] = []
+    var views:[UIView] = []
+    var currentOfSet = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        card.append("Card 1")
+        card.append("Card 2")
+        card.append("Card 2")
+        card.append("Card 2")
+        card.append("Card 2")
+        scrollView.backgroundColor = .red
+        scrollView.clipsToBounds = false
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contendWidth:CGFloat = 0.0
+        
+        for x in 0...card.count {
+            let cardView = UIView()
+            cardView.backgroundColor = .black
+            cardView.layer.cornerRadius = 5
+            
+            cardView.frame = CGRect(x: contendWidth + 5, y: 10, width: self.view.frame.size.width - 110, height: 200)
+            self.views.append(cardView)
+            scrollView.addSubview(cardView)
+            contendWidth += self.view.frame.size.width - 100
+
+        }
+        scrollView.contentSize = CGSize(width: contendWidth, height: self.scrollView.frame.size.height)
+            views[1].transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
 
+extension ViewController : UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset)
+        
+        let pageWidth = scrollView.frame.size.width
+        let fractionalPage = scrollView.contentOffset.x / pageWidth
+        let  page = Int(fractionalPage.rounded())
+        
+        if page == 0 {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.views[page].transform = .identity
+            })
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                self.views[page + 1].transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            })
+        }else  {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.views[page].transform = .identity
+            })
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                self.views[page - 1].transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            })
+            
+            if page == views.count - 1 {
+                
+            }else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.views[page + 1].transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+                })
+            }
+        }
+        
+        print(page)
+        
+    }
+    
 }
 
